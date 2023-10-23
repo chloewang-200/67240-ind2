64e803051dc2ca161a8b82cfce7ad29543a93a47
chloewang-200 <84855841+chloewang-200@users.noreply.github.com>
2023-10-23 02:13:32 -0400

    added new blogs, one blog left

--- posts/postData.js
+++ posts/postData.js
@@ -1,59 +1,66 @@
 const blogPostsData = [
     {
         image: "posts/components/mazeCity/imgs/mazeCity.png",
-        title: "Utopian Maze Generator with A* Pathfinding Algorithm",
+        title: "Utopian Maze Generator with Pathfinding Algorithms",
         date: "2023-10-19",
-        intro: "Maze city is a maze generator that uses the A* pathfinding algorithm to generate a maze with a start and end point.",
+        intro: "Generating and navigating 3D mazes uses pathfinding algorithms.",
         link: "posts/components/mazeCity/index.html"
     },
     {
         image: "posts/components/pattern/imgs/tree1.png",
         title: "Shape Grammar and Generative Pattern",
         date: "2023-10-18",
-        intro: "Pattern Finding",
+        intro: "An automatic pattern generator using Shape Grammar (a system of rewriting shape rules).",
         link: "posts/components/pattern/index.html"
     },
     {
         image: "posts/components/floorPlan/imgs/floor.gif",
-        title: "AI Automation of Floor Plan Generation ",
-        date: "2023-10-17",
-        intro: "This is the introduction to Blog Title 3.",
+        title: "A Reduction to the Floor Plan Generation Problem",
+        date: "2022-05-17",
+        intro: "An exploration of reducing floor plan generation problem to box stacking problem.",
         link: "posts/components/floorPlan/index.html"
     },
     {
         image: "posts/components/cellColoring/imgs/cell1.png",
         title: "Reimagining Singel-Cell Coloring Through Machine Learning",
         date: "2023-10-17",
-        intro: "This is the introduction to Blog Title 3.",
+        intro: "This project studies how machine learning algorithms can be used to color single-cell images.",
         link: "posts/components/cellColoring/index.html"
     },
     {
         image: "posts/components/treeGeneration/imgs/tree1.png",
         title: "Tree Structure Generation with L-Systems",
-        date: "2023-2-17",
-        intro: "This is the introduction to Blog Title 3.",
+        date: "2022-12-17",
+        intro: "L-Systems are a mathematical formalism for modeling the growth of plants. This project explores how to generate tree structures using L-Systems.",
         link: "posts/components/treeGeneration/index.html"
     },
     {
         image: "posts/components/childrenPainting/imgs/image16.gif",
         title: "Machine Learning in Recreation of Naive Art",
         date: "2023-10-17",
-        intro: "This is the introduction to Blog Title 3.",
+        intro: "How might we leverage machine learning to craft images that are “incomplete”, “childish”, and “naive”, aiming to stir the very emotions we seek?",
         link: "posts/components/childrenPainting/index.html"
     },
     {
         image: "images/img14.PNG",
         title: "Digital Fabrication and Fashion: Reimagining material with Laser Cutting",
-        date: "2023-05-25",
-        intro: "This is the introduction to Blog Title 3.",
-        link: "link-to-blog3.html"
+        date: "2022-04-25",
+        intro: "Designing at Fashion show: Lunar Gala Aunnual Runway at CMU",
+        link: "posts/components/lunarGala/index.html"
     },
     {
         image: "posts/components/cybernetic/imgs/img8.png",
-        title: "Cybernetics: Does Machine Breath?",
+        title: "Cybernetics and Programmable Air: Does Machine Breath?",
         date: "2023-06-25",
-        intro: "This is the introduction to Blog Title 3.",
+        intro: "This project takes a look at the topics of cybernetics, fluid spaces and programmable air.",
         link: "posts/components/cybernetic/index.html"
+    },
+    {
+        image: "posts/components/music/imgs/warriors.png",
+        title: "Computer Music: Mogolian Warriors",
+        date: "2023-03-25",
+        intro: "I explored music composition using Nyquist (a programming language for sound synthesis and music composition). ",
+        link: "posts/components/music/index.html"
     }
     
 ];

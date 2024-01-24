51876a69329160f8eab54e4d2db4b4fde10b031d
chloewang-200 <84855841+chloewang-200@users.noreply.github.com>
2024-01-24 10:47:47 -0500

    modified links

--- components/goUpButton.js
+++ components/goUpButton.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > document.body.scrollHeight / 4 || document.documentElement.scrollTop > document.documentElement.scrollHeight / 2) {
+  if (document.body.scrollTop > document.body.scrollHeight / 4 || document.documentElement.scrollTop > document.documentElement.scrollHeight / 3) {
     document.getElementById("goUpButton").style.display = "block";
   } else {
     document.getElementById("goUpButton").style.display = "none";

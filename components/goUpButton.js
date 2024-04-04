faa6d2006cbf3d29aadbfe636eca2acec68d10fc
chloewang-200 <84855841+chloewang-200@users.noreply.github.com>
2024-04-04 16:34:18 -0400

    adding chloe bot server

--- components/goUpButton.js
+++ components/goUpButton.js
@@ -1,11 +1,21 @@
 window.onscroll = function() {scrollFunction()};
+var codingSection = document.getElementById("codingSection");
 
+var chatting = false
 function scrollFunction() {
-  if (document.body.scrollTop > document.body.scrollHeight / 4 || document.documentElement.scrollTop > document.documentElement.scrollHeight / 3) {
+  if (document.documentElement.scrollTop > document.documentElement.scrollHeight / 3) {
     document.getElementById("goUpButton").style.display = "block";
   } else {
     document.getElementById("goUpButton").style.display = "none";
-  }
+  } 
+  // if (codingSection.getBoundingClientRect().top <= 50) {
+  //   talkButton.style.display = "none";
+  // } else {
+  //   if (chatting == false){
+  //     talkButton.style.display = "block";
+  //   }
+      
+  // }
 }
 
 document.getElementById("goUpButton").addEventListener("click", function(){
@@ -16,3 +26,25 @@ document.getElementById("goUpButton").addEventListener("click", function(){
         behavior: 'smooth'
     });
 });
+
+
+document.getElementById("talkButton").addEventListener("click", function() {
+  var chatBox = document.getElementById("iframe-container");
+  if (chatBox.style.display === "none") {
+    chatBox.style.display = "block";
+    talkButton.style.display = "none";
+    chatting = true
+  } else {
+    chatBox.style.display = "none";
+
+  }
+});
+
+
+  document.getElementById('close-button').addEventListener('click', function() {
+      // Hide the container
+      document.getElementById('iframe-container').style.display = 'none';
+      // chatting = false;
+      talkButton.style.display = "block";
+  });
+
